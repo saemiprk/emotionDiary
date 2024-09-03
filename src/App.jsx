8e46877cef88bbe9f4f01c00f435a658cbdee8e8
Saemi Park <saemiprk@gmail.com>
2024-09-03 16:32:49 +0900

    diary

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import New from './pages/New'
+import Diary from './pages/Diary'
+import Notfound from './pages/Notfound'
+import Edit from './pages/Edit'
+import { useReducer, useRef } from 'react'
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용"
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용"
+  }
+];
+
+function reducer(state, action){
+  switch(action.type){
+    case 'CREATE': 
+      return [action.data, ...state];
+    case 'UPDATE':
+      return state.map((item) => String(item.id) === String(action.data.id)? action.data : item);
+    case 'DELETE':
+      return state.filter((item) => String(item.id) !== String(action.id));
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content
+      }
+    })
+  }
+
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: 'UPDATE',
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content
+      }
+
+    })
+  }
+
+  const onDelete = (id) => {
+    dispatch({
+      type: 'DELETE',
+      id
+    })
+  }
+
+  return (
+    <>    
+    <button onClick={() => onCreate(new Date().getTime(), 1, "아무거나")}>일기 추가 TEST</button>
+    <button onClick={() => onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다.")}>일기 수정 TEST</button>
+    <button onClick={() => onDelete(1)}>일기 삭제 TEST</button>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/new' element={<New />} />
+      <Route path='/diary/:id' element={<Diary />} />
+      <Route path='/edit/:id' element={<Edit />} />
+      <Route path='*' element={<Notfound />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Diary.jsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom";
+
+const Diary = () => {
+    const params = useParams();    
+    return <div>{params.id} Diary</div>
+}
+
+export default Diary;
